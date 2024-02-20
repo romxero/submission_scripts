91e6c84231de13aa868fb93ea0f72ca5f3b4396a
RC <randall.white@czbiohub.org>
2024-02-20 15:43:37 -0800

    scripts

---
+++ spack_build_slurm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --time=4-00:00:00
+#SBATCH -c 2 
+#SBATCH -J "spack_environment_builds"
+#SBATCH -p cpu
+#SBATCH -N 1
+#SBATCH --mem=4G
+#SBATCH --output=spack_environment_builds_%A.%a.out
+#SBATCH --error=spack_environment_builds_%A.%a.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=randall.white@czbiohub.org
+#SBATCH -D /home/randall.white/hpc/spack 
+
+
+# after slurm environment set, lets invoke environments or set modules:
+
+ml purge
+
+git clone git@github.com:romxero/spack_cz_tester.git
+
+cp spack_cz_tester/cz_spack.yml
+
+ml load gcc/11.3.0
+
+source spack/share/spack/setup-env.sh
+
+spack env activate myenv
+
+spack install 
+
+# now we direct spack to install some things:
+
+spack env deactivate
+
+exit 0 
---
+++ start_spid_jupyter.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+## 02/15/24
+## start spid jupyter
+
+#note that the container already pretty much creates the directories, just make sure to bind `/hpc` and pass --nv to use gpus
+
+#container location
+CONTAINER_FULL_PATH='/hpc/apps/spid/master/bin/spid_container.sif'
+
+#noteboook location to launch everything 
+NOTEBOOK_ROOT=${HOME}
+
+function create_random_port () 
+{
+
+    local MAX_PORT=65535
+    local SET_PORT_START=60000
+
+    PORT_DIFFERENCE=$((${MAX_PORT} - ${SET_PORT_START}))
+    VALS=($(seq 60000 65535))
+
+    echo ${VALS[$((($RANDOM + $PORT_DIFFERENCE) % 5535))]}
+
+}
+
+
+
+MY_PORT=$(create_random_port)
+
+# create server config
+export CONFIG_FILE="${PWD}/config.py"
+(
+umask 077
+cat > "${CONFIG_FILE}" << EOL
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = ${MY_PORT}
+c.NotebookApp.port_retries = 0
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.notebook_dir = "${NOTEBOOK_ROOT}"
+c.NotebookApp.disable_check_xsrf = True
+EOL
+)
+
+
+echo "Starting SPID Container w/ Jupyter Notebook"
+
+apptainer instance start --nv --bind /hpc:/hpc ${CONTAINER_FULL_PATH} jupyter notebook --config="${CONFIG_FILE}"
+
+exit 0 
+
