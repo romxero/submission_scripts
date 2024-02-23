9097b833cc88b379745f461029dfbb9a002626d9
RC <randall.white@czbiohub.org>
2024-02-23 14:54:24 -0800

    new spackbuilds scripting for flux on bruno

--- spack_build_slurm.sh
+++ spack_build_slurm.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
-#SBATCH --time=4-00:00:00
-#SBATCH -c 2 
+#SBATCH --time=1-00:00:00
+#SBATCH -c 4
 #SBATCH -J "spack_environment_builds"
-#SBATCH -p cpu
+#SBATCH -p preempted,cpu
 #SBATCH -N 1
-#SBATCH --mem=4G
+#SBATCH --mem-per-cpu=2G
 #SBATCH --output=spack_environment_builds_%A.%a.out
 #SBATCH --error=spack_environment_builds_%A.%a.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=randall.white@czbiohub.org
 #SBATCH -D /home/randall.white/hpc/spack 
 
+# the test environment name 
+SPACK_ENV_NAME="MY_TEST_ENVIRONMENT"
 
-# after slurm environment set, lets invoke environments or set modules:
-
+# modules 
 ml purge
+ml load gcc/11.3
 
+# git clone and grab the configuration file 
 git clone git@github.com:romxero/spack_cz_tester.git
-
 cp spack_cz_tester/cz_spack.yml .
-
-ml load gcc/11.3.0
 
 source spack/share/spack/setup-env.sh
 
-spack env activate myenv ./cz_spack.yml
+spack compiler find /hpc/apps/x86_64/gcc/11.3
 
-spack install 
+spack env activate -p ${SPACK_ENV_NAME} ./cz_spack.yml
 
-# now we direct spack to install some things:
+spack install -J ${SLURM_CPUS_PER_TASK}
 
+# END OF SCRIPT
 spack env deactivate
 
 exit 0 
