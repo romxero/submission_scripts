0bb36a2e82023166cfc281ea30a4b4977b4f276a
RC <randall.white@czbiohub.org>
2024-03-27 16:28:03 -0700

    long day

--- relion_build.sh
+++ relion_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=relion_build
 #SBATCH --partition=preempted,gpu
-#SBATCH --time=04:00:00
+#SBATCH --time=01:30:00
 #SBATCH --gpus-per-task=1
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
@@ -110,7 +110,7 @@ exit 0
     make -j 8
 
     make install 
-    
+
     popd 
 
     }
