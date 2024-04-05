2dab09d239df347a4b7dcf138c3c3527faefdeea
RC <randall.white@czbiohub.org>
2024-04-05 14:30:46 -0700

    tracking the conversion of the markdown to docx for confluence uploads

---
+++ to_docx_wut.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+# we are going to try and convert everythign to docx
+
+
+function convert_to_docx() {
+    local file=$1
+    local docx_file=${file%.md}.docx
+    echo "Converting $file to $docx_file"
+    pandoc -s $file -o $docx_file
+}
+
+
+export -f convert_to_docx
+
+
+find . -name "*.md" -print | xargs -I '{}' bash -c 'convert_to_docx "{}"'
+
