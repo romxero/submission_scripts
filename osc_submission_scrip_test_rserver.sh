8594e9168693af7637c86b42f79411e2b7664a92
RC <randall.white@czbiohub.org>
2024-06-14 14:42:18 -0700

    update

--- osc_submission_scrip_test_rserver.sh
+++ osc_submission_scrip_test_rserver.sh
@@ -7,7 +7,7 @@ echo "Waiting for RStudio Server to open port ${port}..."
 echo "TIMING - Starting wait at: $(date)"
 
 setup_env () {
-  export RSTUDIO_SERVER_IMAGE="/home/aymane/Scripts/RStudio/rstudio-server.sif"
+  export RSTUDIO_SERVER_IMAGE="/hpc/mydata/randall.white/rstudio_apptainer_build/rstudio_apptainer/this.sif"
   export SINGULARITY_BINDPATH="/etc,/media,/mnt,/opt,/srv,/usr,/var"
   export PATH="$PATH:/usr/lib/rstudio-server/bin"
 }
@@ -17,22 +17,22 @@ setup_env
 #MY_PRE_TMP=$(mktemp -d -t rstudio_${USER}_server-XXXX)
 
 export MY_TMP_DIR=$(mktemp -d -t rstudio_${USER}_server-XXXX)
-mkdir -p $MY_TMP_DIR/var/lib/rstudio-server
-mkdir -p $MY_TMP_DIR/var/run/rstudio-server
+mkdir -p ${MY_TMP_DIR}/var/lib/rstudio-server
+mkdir -p ${MY_TMP_DIR}/var/run/rstudio-server
 
-cat /proc/sys/kernel/random/uuid > "$MY_TMP_DIR/var/run/rstudio-server/secure-cookie-key"
-chmod 0600 "$MY_TMP_DIR/var/run/rstudio-server/secure-cookie-key"
+cat /proc/sys/kernel/random/uuid > "${MY_TMP_DIR}/var/run/rstudio-server/secure-cookie-key"
+chmod 0600 "${MY_TMP_DIR}/var/run/rstudio-server/secure-cookie-key"
 
-export RSTUDIO_DB_FILE="$MY_TMP_DIR/var/lib/rstudio-server/rstudio-os.sqlite"
-touch $RSTUDIO_DB_FILE
-chmod 0600 $RSTUDIO_DB_FILE
+export RSTUDIO_DB_FILE="${MY_TMP_DIR}/var/lib/rstudio-server/rstudio-os.sqlite"
+touch ${RSTUDIO_DB_FILE}
+chmod 0600 ${RSTUDIO_DB_FILE}
 
 echo "Starting up rserver..."
 
-singularity exec --bind $MY_TMP_DIR/var/lib/rstudio-server:/var/lib/rstudio-server \
-  --bind $MY_TMP_DIR/var/run/rstudio-server:/var/run/rstudio-server \
+apptainer exec --bind ${MY_TMP_DIR}/var/lib/rstudio-server:/var/lib/rstudio-server \
+  --bind ${MY_TMP_DIR}/var/run/rstudio-server:/var/run/rstudio-server \
   --home /home/oodadmin:/home/oodadmin \
-  $RSTUDIO_SERVER_IMAGE \
+  ${RSTUDIO_SERVER_IMAGE} \
   /usr/lib/rstudio-server/bin/rserver \
   --auth-none 1 \
   --www-port=${port} \
@@ -43,25 +43,26 @@ sleep 5
 
 echo 'Singularity has exited...'
 
-echo "Waiting for RStudio Server to open port ${port}..."
-timeout 60 sh -c 'until nc -z localhost ${port}; do sleep 1; done'
-
-if ! nc -z localhost ${port}; then
-  echo "Timed out waiting for RStudio Server to open port ${port}!"
-  
-  echo "Capturing log files for debugging:"
-  if [ -f "${MY_TMP_DIR}/rsession.log" ]; then
-    echo "Content of rsession.log:"
-    cat "${MY_TMP_DIR}/rsession.log"
-  else
-    echo "rsession.log not found."
-  fi
-  
-  if [ -f "${HOME}/.local/share/rstudio/log/rserver.log" ]; then
-    echo "Content of rserver.log:"
-    cat "${HOME}/.local/share/rstudio/log/rserver.log"
-  fi
-  exit 1
-else
-  echo "RStudio Server is running on port ${port}"
-fi
+# deprecated below
+#echo "Waiting for RStudio Server to open port ${port}..."
+#timeout 60 sh -c 'until nc -z localhost ${port}; do sleep 1; done'
+#
+#if ! nc -z localhost ${port}; then
+#  echo "Timed out waiting for RStudio Server to open port ${port}!"
+#  
+#  echo "Capturing log files for debugging:"
+#  if [ -f "${MY_TMP_DIR}/rsession.log" ]; then
+#    echo "Content of rsession.log:"
+#    cat "${MY_TMP_DIR}/rsession.log"
+#  else
+#    echo "rsession.log not found."
+#  fi
+#  
+#  if [ -f "${HOME}/.local/share/rstudio/log/rserver.log" ]; then
+#    echo "Content of rserver.log:"
+#    cat "${HOME}/.local/share/rstudio/log/rserver.log"
+#  fi
+#  exit 1
+#else
+#  echo "RStudio Server is running on port ${port}"
+#fi
