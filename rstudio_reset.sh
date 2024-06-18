9bbb8f6cd90922493197c80e69bef481190670cb
RC <randall.white@czbiohub.org>
2024-06-18 11:38:23 -0700

    changed to add conditionals into this script to be safe

--- rstudio_reset.sh
+++ rstudio_reset.sh
@@ -4,15 +4,45 @@
 ##
 ##
 ml purge 
+
+# please change this to the respective R version. 
 ml load R/4.3 
 
 pushd ~
 # remove stored data file and history
-rm -rf .RData .Rhistory
+# remove the Rstudio-Desktop state file
+
+if [ -f .RData ]; then
+  rm .RData
+fi
+
+if [ -f .Rhistory ]; then
+  rm .Rhistory
+fi
+
 
 # move the Rstudio config files to a backup location
-mv ~/.local/share/rstudio ~/.local/share/rstudio-backup
+
+if [ -d ~/.local/share/rstudio ]; then
+  mv ~/.local/share/rstudio ~/.local/share/rstudio-backup
+fi
+
+
+
+if [ -d ~/.config/RStudio ]; then
+  
 mv ~/.config/RStudio ~/backup-RStudio
+
+fi
+
+
+
+if [ -d ~/.config/rstudio ]; then
+  
+mv ~/.config/rstudio/ ~/backup-rstudio
+
+fi
+
 
 # run this method to clean up the Rstudio session
 Rscript <(echo "gc(); gctorture(FALSE);")
