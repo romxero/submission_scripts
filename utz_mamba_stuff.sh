d9759ea65d97088b3ba72fddac0979c1a93ec842
RC <randall.white@czbiohub.org>
2024-07-15 10:25:11 -0700

    utz stuff for mamba for testing cachefilesd

---
+++ utz_mamba_stuff.sh
@@ -0,0 +1,14 @@
+module load mamba
+mamba env list
+mamba env list
+mamba activate album
+module load mamba
+mamba create -n dev
+mamba activate dev
+mamba install -c conda-forge cmake==3.23
+mamba env list
+module load mamba
+mamba env list
+mamba activate dev
+mamba install python==3.11 pybind11 scikit-build
+mamba install numpy
