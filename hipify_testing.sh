292ddc3621f2a80ba50e26080607568436f3fa2a
RC <randall.white@czbiohub.org>
2024-07-26 14:26:10 -0700

    updated the hipify script

--- hipify_testing.sh
+++ hipify_testing.sh
@@ -1,32 +1,75 @@
 #!/bin/bash
-
 # This script is used to find all the CUDA files in the motioncorr project
 MAIN_PROJ_DIR="/home/randall.white/mydata/motioncorr_dev_work"
 MY_CUDA_PATH="/hpc/apps/x86_64/cuda/12.4.0_550.54.14"
 
-pushd ${MAIN_PROJ_DIR}
+function gen_env_dirs()
+{
 
-source spack/share/spack/setup-env.sh
+    MY_CURR_DIR=$(pwd)
 
-spack load hipify-clang@6.1.2
-spack load llvm-amdgpu
+    MY_WORK_DIR="./hip_work" 
 
-
-# spack find -H -p llvm-amdgpu | cut -d ' ' -f 3
-LLVM_DIR=$(spack location -i llvm-amdgpu)
-
-hipify-clang --clang-resource-directory
-
-hipify-clang --clang-resource-directory=$LLVM_DIR/lib/clang/17 GCC2D.cu -o test.cu
-
-hipify-clang --cuda-path=${MY_CUDA_PATH} -I ./MotionCor3/Util -I ./MotionCor3/LibSrc/Include --clang-resource-directory=$LLVM_DIR/lib/clang/17 --cuda-path= GFFT1D.cu -o test.cu
-
-#this is working :
-hipify-clang --cuda-path=${MY_CUDA_PATH} -I ./MotionCor3/Util -I ./MotionCor3/LibSrc/Include --clang-resource-directory=$LLVM_DIR/lib/clang/17 --cuda-path= GFFT1D.cu -o test.cu
+    mkdir -p ${MY_WORK_DIR}
 
 
 
-#<()
+}
 
-#find . -name "*.cu" -print |  wc -l
+function enter_directory_and_download_spack_and_install_amd()
+{
+    pushd ${MY_WORK_DIR}
+    
+    git clone -c feature.manyFiles=true https://github.com/spack/spack.git
 
+    source spack/share/spack/setup-env.sh
+    
+    spack install hipify-clang
+    
+    spack install rocm-cmake    
+
+}
+
+
+function load_spack_env()
+{
+    spack load hipify-clang@6.1.2
+    spack load llvm-amdgpu
+
+}
+
+
+
+
+function compile_the_project_with_hipify()
+{
+
+    hipify-clang --clang-resource-directory
+
+    hipify-clang --clang-resource-directory=$LLVM_DIR/lib/clang/17 GCC2D.cu -o test.cu
+
+    hipify-clang --cuda-path=${MY_CUDA_PATH} -I ./MotionCor3/Util -I ./MotionCor3/LibSrc/Include --clang-resource-directory=$LLVM_DIR/lib/clang/17 --cuda-path= GFFT1D.cu -o test.cu
+
+    #this is working :
+    hipify-clang --cuda-path=${MY_CUDA_PATH} -I ./MotionCor3/Util -I ./MotionCor3/LibSrc/Include --clang-resource-directory=$LLVM_DIR/lib/clang/17 --cuda-path= GFFT1D.cu -o test.cu
+
+}
+
+
+function main()
+{
+    gen_env_dirs
+    enter_directory_and_download_spack
+    load_spack_env
+
+    # make sure we get the LLVM directory 
+    LLVM_DIR=$(spack location -i llvm-amdgpu)
+
+
+}
+
+
+# main execution 
+main 
+##
+##
